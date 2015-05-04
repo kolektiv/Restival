99691cee85a061f372e0b355b6529bc5c9e39a5a
kolektiv <andrew@xyncro.com>
2015-05-04 23:21:21 +0100

    basic api for a greeting. low level, verbose for this many resources! no idea how to wire it up...

--- src/Restival.Api.Freya/Hello.fs
+++ src/Restival.Api.Freya/Hello.fs
@@ -1,4 +1,74 @@
 ﻿module Restival.Api.Freya
 
+open System.Text
+open Chiron
+open Freya.Core
+open Freya.Core.Operators
+open Freya.Machine
+open Freya.Machine.Extensions.Http
+open Freya.Machine.Router
+open Freya.Router
+open Freya.Types.Http
+open Freya.Types.Uri.Template
+
+// Types
+
+type Greeting =
+    { Message: string }
+
+    static member ToJson (x: Greeting) =
+        Json.write "message" x.Message
+
+// Encoding
+
+let inline encode resource =
+    resource
+    |> Json.serialize
+    |> Json.format
+    |> Encoding.UTF8.GetBytes
+
+// Properties
+
+let greeting =
+        fun name ->
+            match name with
+            | Some name -> { Message = sprintf "Hello %s!" name }
+            | _ -> { Message = "Hello World!" }
+    <!> Freya.getLensPartial (Route.atom "name")
+
+// Resources
+
+let greetingOk _ =
+        fun greeting ->
+            { Data = encode greeting
+              Description =
+                { Charset = Some Charset.Utf8
+                  Encodings = None
+                  MediaType = Some MediaType.Json
+                  Languages = None } }
+    <!> greeting
+
+let greetingResource =
+    freyaMachine {
+        using http
+        handleOk greetingOk } |> FreyaMachine.toPipeline
+
+// Routes
+
+let root =
+    UriTemplate.Parse "/hello"
+
+let routes =
+    freyaRouter {
+        resource (root) greetingResource
+        resource (root + UriTemplate.Parse "/{name}") greetingResource } |> FreyaRouter.toPipeline
+
+// App
+
+let app =
+    OwinAppFunc.ofFreya routes
+
+// Startup (TODO)
+
 type Hello () =
     member __.X = "F#"
